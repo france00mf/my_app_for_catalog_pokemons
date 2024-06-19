33c97b165329c2f3b018aa282fc015c0f4138e29
France! <franciscodomingosjose06798@gmail.com>
2024-06-19 20:45:30 +0100

    add categoryEntites and minor changes

---
+++ lib/domain/entities/category_entities.dart
@@ -0,0 +1,10 @@
+
+import 'dart:ui';
+
+class CategoryEntity{
+  final Color color;
+  final String name;
+  final Routes route;
+
+  CategoryEntity({required this.color, required this.name, required this.route});
+}
