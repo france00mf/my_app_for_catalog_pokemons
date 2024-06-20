0a557fd51a572ab7a16b7a9b697245a1f2aab685
France! <franciscodomingosjose06798@gmail.com>
2024-06-20 10:52:07 +0100

    add others method on localDatasource

--- lib/data/datasources/local/local_datasource.dart
+++ lib/data/datasources/local/local_datasource.dart
@@ -8,7 +8,7 @@ class LocalDataSource{
    Hive.registerAdapter<PokemonHiveModel>();
    Hive.registerAdapter<PokemonGenderHiveModel>();
    Hive.registerAdapter<PokemonsStatsHiveModel>()
-   
+   Hive.registerAdapter<ItemHiveModel>();
   }
 
   Future<bool> hasData() async{
@@ -16,7 +16,36 @@ class LocalDataSource{
   }
 
 
-  savePokemons(){
+  Future<void> savePokemons()async{
 
-  } 
+  }
+
+  Future<List<PokemonsHiveModel>> getAllPokemons()async{
+
+  }
+
+  Future<List<PokemonHiveModel>> getPokemons({required int page, required int limit}){
+
+  }
+
+  Future<PokemonHiveModel?> getPokemon(String number)async{
+    final pokemon = Hive.box<PokemonHiveModel>(PokemonHiveModel.boxKey);
+  }
+
+  Future<void> getEvolutions(PokemonHiveModel pokemon) async{
+
+  }
+
+  Future<void> saveItems(Iterable<ItemHiveModel> items)async{
+
+  }
+
+  Future<List<ItemHiveModel>> getAllItems() async{
+
+  }
+
+  Future<List<ItemHiveModel>> getItem({required int page, required int limit}){
+    
+  }
+
 }
