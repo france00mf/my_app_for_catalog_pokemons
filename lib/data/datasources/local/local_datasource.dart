55884130b8e54591f30c7753ae8de3deab8addd4
France! <franciscodomingosjose06798@gmail.com>
2024-06-20 10:38:01 +0100

    add localDatasources and methods with Hive initialization

--- lib/data/datasources/local/local_datasource.dart
+++ lib/data/datasources/local/local_datasource.dart
@@ -1,12 +1,22 @@
 
+import 'package:hive/hive.dart';
+
 class LocalDataSource{
   static Future<void> initialize()async{
+   await Hive.initFlutter();
 
+   Hive.registerAdapter<PokemonHiveModel>();
+   Hive.registerAdapter<PokemonGenderHiveModel>();
+   Hive.registerAdapter<PokemonsStatsHiveModel>()
+   
   }
 
   Future<bool> hasData() async{
 
   }
 
-  
+
+  savePokemons(){
+
+  } 
 }
